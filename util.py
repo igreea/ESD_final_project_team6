127eceeba9894dadee6293167be2c08c890aa335
igreea <igreea605@gmail.com>
2025-05-02 17:37:31 +0900

    onnx optimization

--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np
 import threading
 from queue import Queue, Empty
+from ultralytics import YOLO
 
 # 해상도 설정
 HIGH_RES = (1280, 1280)
@@ -19,6 +20,24 @@ det_queue = Queue(maxsize=1)
 
 stop_event = threading.Event()
 
+def load_onnx_model(model_path):
+    """
+    Load the ONNX model using ONNX Runtime.
+    :param model_path: Path to the ONNX model file.
+    :return: ONNX Runtime session object.
+    """
+    try:
+        model = YOLO(model_path)
+        model.export(format="onnx")
+        onnx_name = model_path.split(".")[0] + ".onnx"
+        onnx_model = YOLO(onnx_name)
+        if not onnx_model is None:
+            print("ONNX model generated successfully.")
+        return onnx_model
+    except Exception as e:
+        print(f"Error loading ONNX model: {e}")
+        return None
+    
 def picam2_init():
     """
     Picamera2 초기화 및 설정
